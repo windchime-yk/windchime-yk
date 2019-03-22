0e1b0ddabb770494898155b02e212e906318e777
windchime-yk <windchimeyk@gmail.com>
2019-03-22 13:43:50 +0900

    modify: edit global navigation

--- components/GlobalNav.vue
+++ components/GlobalNav.vue
@@ -1,11 +1,10 @@
 <template>
   <nav role="navigation">
     <ul class="list">
-      <li class="item"><a href="/" class="link">Top</a></li>
-      <li class="item"><a href="/about" class="link">About</a></li>
-      <li class="item"><a href="/product" class="link">Product</a></li>
-      <li class="item"><a href="/blog" class="link">Blog</a></li>
-      <li class="item"><a href="/contact" class="link">Contact</a></li>
+      <li class="item"><a href="#about" class="link">About</a></li>
+      <li class="item"><a href="#product" class="link">Product</a></li>
+      <li class="item"><a href="#blog" class="link">Blog</a></li>
+      <li class="item"><a href="#contact" class="link">Contact</a></li>
     </ul>
   </nav>
 </template>
