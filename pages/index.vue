e2280a73b85785ccafe217ab6d87aa8cf4fc6849
windchime-yk <windchimeyk@gmail.com>
2019-03-22 13:46:43 +0900

    modify: edit index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,77 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        WindoChimeYK Portfolio Site
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <div class="container">
+    <div class="notice__wrap">
+      <p class="notice__text">このサイトは現在試験構築中なので見た目が中途半端です</p>
     </div>
-  </section>
+    <section id="about" class="section__wrap">
+      <h2>About</h2>
+      <p>このサイトはWhyKのポートフォリオサイトです</p>
+      <section>
+        <h3>Why? WhyK</h3>
+        <dl>
+          <dt>WhyK（ほわぁいけー）</dt>
+          <dd>そこらへんのフロントエンドエンジニア</dd>
+          <dd>好きなものはVue SFCとアクセシビリティ</dd>
+          <dd>嫌いなものはメモ帳コーディング</dd>
+          <dd>Figmaでデザイン、VS Codeでコーディングするマン</dd>
+          <dd>好きなタイミングで好きなものを作ろうとして早々に挫折するマン</dd>
+          <dd>GitHubに溜まるdevelopブランチ止まりのレポジトリたち</dd>
+        </dl>
+      </section>
+    </section>
+    <section id="about" class="section__wrap">
+      <h2>Work</h2>
+      <ul>
+        <li>
+          <a href="https://github.com/windchime-yk/novel-support.js">
+            <h2>novel-support.js</h2>
+            <img src="" alt="">
+            <dl>
+              <dt>使用技術</dt>
+              <dd>TypeScript</dd>
+              <dd>Sass</dd>
+            </dl>
+            <p>カクヨムの特殊記法をベースとした小説投稿サイト特殊記法支援パッケージ</p>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/windchime-yk/gas-es6-env">
+            <h2>gas-es6-env</h2>
+            <img src="" alt="">
+            <dl>
+              <dt>使用技術</dt>
+              <dd>webpack</dd>
+              <dd></dd>
+              <dd></dd>
+            </dl>
+            <p>Google Apps ScriptをローカルでES6+を使って制作する環境</p>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <h2>images-to-pdf-cli</h2>
+            <img src="" alt="">
+            <dl>
+              <dt>使用技術</dt>
+              <dd>Node.js</dd>
+            </dl>
+            <p>パッケージ<a href="https://github.com/mLuby/images-to-pdf">images-to-pdf</a>をCLI化したもの</p>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <section id="about" class="section__wrap">
+      <h2>Blog</h2>
+      <p>ここに</p>
+    </section>
+    <section id="about" class="section__wrap">
+      <h2>Contact</h2>
+      <ul>
+        <li><a href="mailto:windchimeyk@gmail.com">メール</a></li>
+        <li><a href="https://twitter.com/windchime_yk">Twitter</a></li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -33,33 +85,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.notice__wrap {
   text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: $mainColor;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  color: white;
+  border: 5px solid red;
+  background: #f00;
+  border-radius: 5px;
+  width: 80%;
+  margin: 10px auto;
+  padding: 20px;
 }
 </style>
 
